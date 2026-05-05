beef3753c4d1bdaf368e29922ac057ebf3db5042
Faizuddin Shaik <faiz.24365@gmail.com>
2026-05-04 22:23:17 -0500

    integrate with aws

---
+++ fetch_price.py
@@ -0,0 +1,27 @@
+import requests
+import boto3
+import json
+
+def get_price():
+    url = "https://hourlypricing.comed.com/api?type=currenthour"
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    
+    response = requests.get(url, headers=headers)
+    data = response.json()
+    return float(data[0]['price'])
+
+def trigger_lambda(price):
+    client = boto3.client('lambda', region_name='us-east-1')
+    payload = {"current_price": price}
+    
+    # Trigger the Lambda we built earlier
+    client.invoke(
+        FunctionName='ComEdPriceChecker',
+        InvocationType='Event', # Async trigger
+        Payload=json.dumps(payload)
+    )
+    print(f"Successfully pushed price {price} to AWS Lambda.")
+
+if __name__ == "__main__":
+    current_price = get_price()
+    trigger_lambda(current_price)
